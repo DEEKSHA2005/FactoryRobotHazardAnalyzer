764804ae810da6a01c725e616a771ca24e8a5346
Deeksha <smdeeksha2005@gmail.com>
2026-01-29 16:00:46 +0530

    [Deeksha]: Added: UC4: Validate inputs using if-else conditions

--- src/FactoryRobotHazardAnalyzer.java
+++ src/FactoryRobotHazardAnalyzer.java
@@ -2,38 +2,55 @@ import java.util.Scanner;
 
 public class FactoryRobotHazardAnalyzer {
 
-        public static void main(String[] args) {
+    public static void main(String[] args) {
 
         System.out.println("Factory Robot Hazard Analyzer");
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter Arm Precision: ");
+        System.out.print("Enter Arm Precision (0.0 - 1.0): ");
         double armPrecision = scanner.nextDouble();
 
-        System.out.print("Enter Worker Density: ");
+        System.out.print("Enter Worker Density (1 - 20): ");
         int workerDensity = scanner.nextInt();
 
         scanner.nextLine();
 
-        System.out.print("Enter Machinery State: ");
+        System.out.print("Enter Machinery State (Worn/Faulty/Critical): ");
         String machineryState = scanner.nextLine();
 
-        double machineryRiskFactor = 1.0;
-
-        if (machineryState.equals("Worn")) {
-            machineryRiskFactor = 1.3;
-        } else if (machineryState.equals("Faulty")) {
-            machineryRiskFactor = 2.0;
-        } else if (machineryState.equals("Critical")) {
-            machineryRiskFactor = 3.0;
+        if (armPrecision < 0.0 || armPrecision > 1.0) {
+            System.out.println("Error: Arm precision must be 0.0-1.0");
         }
 
-        double hazardRiskScore =
-                ((1.0 - armPrecision) * 15.0) +
-                        (workerDensity * machineryRiskFactor);
+        else if (workerDensity < 1 || workerDensity > 20) {
+            System.out.println("Error: Worker density must be 1-20");
+        }
 
-        System.out.println("Robot Hazard Risk Score: " + hazardRiskScore);
+        else if (
+                !machineryState.equals("Worn") &&
+                        !machineryState.equals("Faulty") &&
+                        !machineryState.equals("Critical")
+        ) {
+            System.out.println("Error: Unsupported machinery state");
+        }
+        else {
+            double machineryRiskFactor = 1.0;
+
+            if (machineryState.equals("Worn")) {
+                machineryRiskFactor = 1.3;
+            } else if (machineryState.equals("Faulty")) {
+                machineryRiskFactor = 2.0;
+            } else if (machineryState.equals("Critical")) {
+                machineryRiskFactor = 3.0;
+            }
+
+            double hazardRiskScore =
+                    ((1.0 - armPrecision) * 15.0) +
+                            (workerDensity * machineryRiskFactor);
+
+            System.out.println("Robot Hazard Risk Score: " + hazardRiskScore);
+        }
 
         scanner.close();
     }
